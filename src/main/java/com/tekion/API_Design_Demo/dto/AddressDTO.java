14ff80a5f8777ed498841181236a08ab343b990b
Aayush <tripathiaayush249@gmail.com>
2026-01-20 15:16:58 +0530

    Initial Address Related Commit

--- src/main/java/com/tekion/API_Design_Demo/dto/AddressDTO.java
+++ src/main/java/com/tekion/API_Design_Demo/dto/AddressDTO.java
@@ -1,6 +1,35 @@
 package com.tekion.API_Design_Demo.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Address Data Transfer Object")
 public class AddressDTO {
-    // TODO: Implement AddressDTO
+
+    @Schema(description = "Unique identifier of the address", example = "addr-001")
+    private String addressId;
+
+    @Schema(description = "Customer ID this address belongs to (foreign key)", example = "cust-123")
+    private String customerId;
+
+    @Schema(description = "Street address", example = "123 Main Street")
+    private String street;
+
+    @Schema(description = "City name", example = "San Francisco")
+    private String city;
+
+    @Schema(description = "State or province", example = "California")
+    private String state;
+
+    @Schema(description = "ZIP or postal code", example = "94102")
+    private String zipCode;
+
+    @Schema(description = "Country name", example = "USA")
+    private String country;
 }
 
